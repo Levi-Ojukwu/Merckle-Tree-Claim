0a915b9aafa6c56bb4ee71d038d1462e402f6124
Levi-Ojukwu <ojukwulevichinedu@gmail.com>
2024-04-17 06:27:58 +0100

    Updated

--- test/AbegToken.t.sol
+++ test/AbegToken.t.sol
@@ -1,68 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.9;
+import "../lib/solmate/src/utils/MerkleProofLib.sol";
+import "../lib/solmate/src/tokens/ERC1155.sol";
 
-import {Test, console2, stdJson} from "forge-std/Test.sol";
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
 
-import {Merkle} from "../src/AbegToken.sol";
+contract Merkle is ERC1155 {
+    bytes32 root;
 
-contract AbegTest is Test {
-    using stdJson for string;
-    Merkle public merkle;
-    struct Result {
-        bytes32 leaf;
-        bytes32[] proof;
+    constructor(bytes32 _root) {
+        root = _root;
     }
 
-    struct User {
-        address user;
-        uint amount;
-    }
-    Result public result;
-    User public user;
-    bytes32 root =
-        0x8f0704ea72bc40746e1ba7da33354957b21e9ad4a1df2dd39c959348ec18b837;
-    address user1 = 0x001Daa61Eaa241A8D89607194FC3b1184dcB9B4C;
+    mapping(address => bool) public hasClaimed;
 
-    function setUp() public {
-        merkle = new Merkle(root);
-        string memory _root = vm.projectRoot();
-        string memory path = string.concat(_root, "/merkle_tree.json");
-
-        string memory json = vm.readFile(path);
-        string memory data = string.concat(_root, "/address_data.json");
-
-        string memory dataJson = vm.readFile(data);
-
-        bytes memory encodedResult = json.parseRaw(
-            string.concat(".", vm.toString(user1))
-        );
-        user.user = vm.parseJsonAddress(
-            dataJson,
-            string.concat(".", vm.toString(user1), ".address")
-        );
-        user.amount = vm.parseJsonUint(
-            dataJson,
-            string.concat(".", vm.toString(user1), ".amount")
-        );
-        result = abi.decode(encodedResult, (Result));
-        console2.logBytes32(result.leaf);
-    }
-
-    function testClaimed() public {
-        bool success = merkle.claim(user.user, user.amount, result.proof);
-        assertTrue(success);
-    }
-
-    function testAlreadyClaimed() public {
-        merkle.claim(user.user, user.amount, result.proof);
-        vm.expectRevert("already claimed");
-        merkle.claim(user.user, user.amount, result.proof);
-    }
-
-    function testIncorrectProof() public {
-        bytes32[] memory fakeProofleaveitleaveit;
-
-        vm.expectRevert("not whitelisted");
-        merkle.claim(user.user, user.amount, fakeProofleaveitleaveit);
+    function claim(
+        address _claimer,
+        uint _amount,
+        uint _id,
+        bytes32[] calldata _proof
+    ) external returns (bool success) {
+        require(!hasClaimed[_claimer], "already claimed");
+        bytes32 leaf = keccak256(abi.encodePacked(_claimer, _amount, _id));
+        bool verificationStatus = MerkleProofLib.verify(_proof, root, leaf);
+        require(verificationStatus, "not whitelisted");
+        hasClaimed[_claimer] = true;
+        _mint(_claimer, _amount, _id, memorybytes32  _data);
+        success = true;
     }
 }
